745b38f6c0188c33b3d715f533afbebb20485cf1
immersiveartkamil <immersiveartstudio@gmail.com>
2024-04-19 17:43:08 +0200

    fix

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -2,6 +2,6 @@
     <div class="hero__text">
         <h1>Wykańczanie biurowców firmowych</h1>
     </div>
-    <div class="hero__img"> <img src="//localhost:3000/wp-content/uploads/2024/04/hero.png" alt=""></div>
+    <div class="hero__img"> <img src="http://complete-interior.local/wp-content/uploads/2024/04/hero.png" alt=""></div>
 
 </div>
