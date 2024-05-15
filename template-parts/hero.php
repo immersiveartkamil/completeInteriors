c336a73f7b72f257e06c2bc477124ba5872d6118
immersiveartkamil <155831169+immersiveartkamil@users.noreply.github.com>
2024-05-16 00:23:49 +0200

    fix

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,6 +1,6 @@
 <div class="hero">
     <div class="hero__text">
-        <h3>Kreujemy i </h3>
+        <h3>test i </h3>
         <h3>Wyposażamy </h3>
     </div>
     <div class="hero__img"> <img src="https://completeinterior.pl/wp-content/uploads/2024/04/hero.png" alt=""></div>
